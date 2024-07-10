546d8f5822ebaba3dbe4e25ba6e0c3ca652b4db9
senay <senaykt@gmail.com>
2024-07-10 12:42:44 -0700

    Added real time feature using moment js library

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,24 @@
+function dateUpdate() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  losAngelesElementTime = losAngelesElement.querySelector(".time");
+  losAngelesElementTime.innerHTML = moment()
+    .tz("America/Los-Angeles")
+    .format("h:mm:ss [<small>]A[</small>]");
+  let losAngelesElementDate = losAngelesElement.querySelector(".date");
+  losAngelesElementDate.innerHTML = moment()
+    .tz("America/Los-Angeles")
+    .format("MMM Do YYYY");
 
+  let parisElement = document.querySelector("#paris");
+  parisElementTime = parisElement.querySelector(".time");
+  parisElementTime.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("h:mm:ss [<small>]A[</small>]");
+  let parisElementDate = parisElement.querySelector(".date");
+  parisElementDate.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("MMM Do YYYY");
+}
+
+dateUpdate();
+setInterval(dateUpdate, 1000);
