119c1b992d6e3368e2202d71ecb24b3319ac8674
senay <senaykt@gmail.com>
2024-07-12 20:53:40 -0700

    Added select functionality

--- javascript/index.js
+++ javascript/index.js
@@ -24,7 +24,24 @@ dateUpdate();
 setInterval(dateUpdate, 1000);
 
 function updateCity(event) {
-  let city = event.target.value;
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
 }
 
 let selectElement = document.querySelector("#city");
